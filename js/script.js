0a5519b4819b156a252425c15243732e503e441a
VISHNUVARTHAN U <vvarthan7@gmail.com>
2022-07-09 13:11:53 +0530

    landing page updates

--- js/script.js
+++ js/script.js
@@ -6,3 +6,43 @@ btn.addEventListener('click', () => {
   nav.classList.toggle('flex');
   nav.classList.toggle('hidden');
 });
+
+//Carousel Slider
+
+let slideIndex = 1;
+showSlide(slideIndex);
+
+// change slide with the prev/next button
+function moveSlide(moveStep) {
+  showSlide(slideIndex += moveStep);
+}
+
+// change slide with the dots
+function currentSlide(n) {
+  showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.getElementsByClassName('dot');
+  
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+  // hide all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.add('hidden');
+  }
+
+  // remove active status from all dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('bg-brightRed');
+  }
+
+  // show the active slide
+  slides[slideIndex - 1].classList.remove('hidden');
+
+  // highlight the active dot
+  dots[slideIndex - 1].classList.add('bg-brightRed');
+}
